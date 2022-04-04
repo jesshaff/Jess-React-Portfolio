74a7030544e32ec64dd7f083b1a4c1bbad8d5bd1
jessicahaff@yahoo.com <jessicahaff@yahoo.com>
2022-04-03 22:15:55 -0400

    added images, css, resume details

---
+++ my-app/src/resumeData.js
@@ -0,0 +1,95 @@
+let resumeData = {
+    "imagebaseurl":"https://jesshaff.github.io/Jess-React-Portfolio/",
+    "name": "Jess Haff",
+    "role": "Full Stack Web Developer",
+    "linkedinId":"jessica-haff",
+    "roleDescription": " I am interested in doing freelance work focusing on React.js",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/jessica-haff/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/jesshaff",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"facebook",
+          "url":"https://www.facebook.com/jess.haff.1",
+          "className":"fa fa-facebook"
+        }
+      ],
+    "aboutme":"Hello! My name is Jessica Haff. I am 27 years old and live in Gainesville, GA. I am currently a Project Manager at Loeb Electric. I found my passion for web development when I started looking into building my own website for my online boutique. I decided that a coding bootcamp would be the best way to learn. Despite my background in customer service and my associates degree in business I have always had a passion for technology and learning new skills to advance my technical knowledge. I am currently enrolled in a full stack coding bootcamp at Georgia Tech so that I can become a web developer. Currently, I have learned and practiced HTML, CSS, JavaScript, React, and Github. I am expected to complete my coding bootcamp April 16, 2022.",
+    "address":"Gainesville, GA",
+    "website":"https://github.com/jesshaff",
+    "education":[
+      {
+        "UniversityName":"Georgia Institute of Technology - Online",
+        "specialization":"Full stack web development",
+        "MonthOfPassing":"April",
+        "YearOfPassing":"2022",
+        "Achievements":"Certificate"
+      },
+      {
+        "UniversityName":"University of Phoenix - Online",
+        "specialization":"Business Fundamentals",
+        "MonthOfPassing":"October",
+        "YearOfPassing":"2019",
+        "Achievements":"Associate's Degree"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Loeb Electric",
+        "specialization":"Project Manager",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"",
+        "Achievements":""
+      },
+      {
+        "CompanyName":"Acuity Brands Lighting",
+        "specialization":"Sr. Inside Sales Rep",
+        "MonthOfLeaving":"March",
+        "YearOfLeaving":"2022",
+        "Achievements":"Started as a customer service rep in October 2018, promoted to Inside Sales Rep in February of 2019, and promoted again to Sr. Inside Sales Rep in February 2022."
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Feed Me Food Finder",
+        "description":"webapp",
+        "imgurl":"./public/images/portfolio/feed-me-food-finder-landing-page.png"
+      },
+      {
+        "name":"Weather Dashboard",
+        "description":"webapp",
+        "imgurl":"./public/images/portfolio/weather-dashboard.png"
+      },
+      {
+        "name":"THRVE App",
+        "description":"webapp",  
+        "imgurl":"./public/images/portfolio/thrve-app.png"
+      },
+      {
+        "name":"Employee Tracker",
+        "description":"webapp",
+        "imgurl":"./public/images/portfolio/employee-tracker-main-menu.png"
+      }
+    ],
+  }
+  
+  export default resumeData
